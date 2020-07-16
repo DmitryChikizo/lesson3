007a9ca92fff884f542d3d360cc8686b252636d9
DmitryChikizo <68144529+DmitryChikizo@users.noreply.github.com>
2020-07-16 20:02:05 +0300

    Add files via upload

--- Task2.java
+++ Task2.java
@@ -1,30 +1,31 @@
 package homework.lesson3;
 
 import java.util.Scanner;
-/*Написать программу, вычисляющую и отображающую все числа
+
+public class Task2 {
+    /*Написать программу, вычисляющую и отображающую все числа
 Фибоначчи меньше введённого пользователем целого числа.
 Решить двумя способами: с помощью цикла и с помощью
-рекурсии.*/
-public class Task2 {
-    // 1 1 2 3 5 7 11 18 29
+рекурсии.
+1 1 2 3 5 7 11 18 29*/
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int value = scanner.nextInt();
-        fibonacciCycle(value);
+        calculateFibonacciByCycle(value);
     }
 
-    private static void fibonacciRecursion(int value){
+    private static void calculateFibonacciByRecursion(int value) {
         int value1 = 1;
         int value2 = 0;
         int sum;
         System.out.print(1 + " ");
-        for (int i = 0; i < value; i++){
+        for (int i = 0; i < value; i++) {
             value1 = value2;
         }
-
     }
 
-    private static void fibonacciCycle(int value) {
+    private static void calculateFibonacciByCycle(int value) {
         int value1 = 1;
         int value2 = 0;
         int sum;
@@ -34,7 +35,7 @@ public class Task2 {
             sum = value1 + value2;
             value2 = value1;
             value1 = sum;
-            if (sum > value){
+            if (sum > value) {
                 break;
             } else {
                 System.out.print(sum + " ");
